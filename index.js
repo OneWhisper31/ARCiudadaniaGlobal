fc5683c5a7124fed831072487a276d7bdf2d3f3b
Matias Garcia <64979089+OneWhisper31@users.noreply.github.com>
2023-03-30 16:02:19 -0300

    Boton sonido agregado

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+var onSound = false;
+function OnSound() {
+  onSound = !onSound
+
+  var s = document.getElementById("sound");
+  
+  if(onSound)
+    s.style = "background-color: rgba(255, 255, 255, 1); display: inline;"
+  else
+    s.style = "background-color: rgba(0, 0, 0, 0.6); display: inline;"
+}
+
 window.addEventListener("arjs-nft-loaded", (event) => {
-    var d = document.getElementById("mybutton");
-    d.style = "display: inline;"
-    document.appendChild(d)
-  });
+  var d = document.getElementById("mybutton");
+  d.style = "display: inline;"
+
+  var s = document.getElementById("sound");
+  s.style = "display: inline;"
+
+  // document.appendChild(d + s)
+});
+
+
+window.addEventListener("markerFound", (event) => {
+  if (onSound) {
+  // sonido
+  }
+});
